684d420614b7df9ca15f2d9e9db8fb92813ca294
Jim Burch <jrburch1@gmail.com>
2024-02-03 11:31:06 -0700

    add cover text when team is empty

--- src/components/Team/Team.tsx
+++ src/components/Team/Team.tsx
@@ -19,30 +19,40 @@ export default function Team({ team, setTeam }: TeamProps) {
 
   return (
     <div className={styles.root}>
-      <div className={styles.team}>
-        {Array.from({ length: 5 }).map((_, index) => {
-          const hero = team[index];
-          return (
-            <div key={index} className={styles.hero}>
-              {hero ? (
-                <HeroCard
-                  name={hero.name}
-                  description={hero.description}
-                  imageUrl={`${hero.thumbnail.path}.${hero.thumbnail.extension}`}
-                />
-              ) : (
-                <HeroCard skeleton />
-              )}
-              {hero && (
-                <Button onClick={(e) => handleRemoveFromTeam(e, hero)}>
-                  Disband
-                </Button>
-              )}
-            </div>
-          );
-        })}
+      <div className={styles.teamWrapper}>
+        {!team.length ? (
+          <div className={styles.centeredText}>Recruit Your Heroes</div>
+        ) : null}
+        <div
+          className={styles.team}
+          style={{ opacity: !team.length ? "25%" : "100%" }}
+        >
+          {Array.from({ length: 5 }).map((_, index) => {
+            const hero = team[index];
+            return (
+              <div key={index} className={styles.hero}>
+                {hero ? (
+                  <HeroCard
+                    name={hero.name}
+                    description={hero.description}
+                    imageUrl={`${hero.thumbnail.path}.${hero.thumbnail.extension}`}
+                  />
+                ) : (
+                  <HeroCard skeleton />
+                )}
+                {hero && (
+                  <Button onClick={(e) => handleRemoveFromTeam(e, hero)}>
+                    Disband
+                  </Button>
+                )}
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <p>{`Choose ${5 - team.length} more to complete your team`}</p>
+      {team.length && team.length < 5 ? (
+        <p>{`Choose ${5 - team.length} more to complete your team`}</p>
+      ) : null}
     </div>
   );
 }
